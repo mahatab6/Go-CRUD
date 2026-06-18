9f9fdd3b3bbf09246e9b15b4cac2e0a468f074ad
mahatav6 <mahatabcall56@gmail.com>
2026-06-18 08:57:30 +0600

    user update

--- main.go
+++ main.go
@@ -36,6 +36,7 @@ func main() {
 	mux.HandleFunc("POST /createUser", createHandler)
 	mux.HandleFunc("GET /users", getUsersHandler)
 	mux.HandleFunc("GET /users/{id}", getSingleUsersHandler)
+	mux.HandleFunc("PUT /users/{id}", updateUsersHandler)
 	fmt.Println("Server is running at port 5000")
 	err := http.ListenAndServe(":5000", mux)
 	if err != nil {
@@ -97,6 +98,45 @@ func getSingleUsersHandler(w http.ResponseWriter, r *http.Request) {
 		if user.Id == id {
 			w.Header().Set("Content-Type", "application/json")
 			json.NewEncoder(w).Encode(user)
+			return
 		}
 	}
+
+	w.WriteHeader(http.StatusBadRequest)
+	fmt.Fprintln(w, "User not found")
+}
+
+func updateUsersHandler(w http.ResponseWriter, r *http.Request) {
+	idParam := r.PathValue("id")
+
+	id, err := strconv.Atoi(idParam)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintln(w, "Invalid user id")
+		return
+	}
+
+	var updatedUser User
+
+	err = json.NewDecoder(r.Body).Decode(&updatedUser)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintln(w, "Invalid requset body")
+		return
+	}
+
+	for i, user := range users {
+		if user.Id == id {
+			updatedUser.Id = id
+			users[i] = updatedUser
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(updatedUser)
+			return
+		}
+	}
+
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprintln(w, "User not found")
 }
